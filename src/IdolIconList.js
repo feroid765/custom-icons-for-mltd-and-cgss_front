ccc9b5c4b512d95d2c68da2e0c4e86b79e470b6d
박광호 <feroid@baggwanghoui-MacBookPro.local>
2020-01-27 00:43:32 +0900

    Change a source of mltd images

--- src/IdolIconList.js
+++ src/IdolIconList.js
@@ -1,53 +1,59 @@
-import React, {useState} from 'react';
-import {Card, Image, Button, Pagination} from 'semantic-ui-react';
-import './IdolIconList.css';
+import React, { useState } from "react";
+import { Card, Image, Button, Pagination } from "semantic-ui-react";
+import "./IdolIconList.css";
 
 const baseURL_cgss = "https://hidamarirhodonite.kirara.ca/icon_card/";
-const baseURL_mltd = "https://storage.matsurihi.me/mltd/icon_l/";
+const baseURL_mltd = "https://custom-icon.feroid.com/img/mltd/";
 
-function idolIcon(key, type){
-    return(
+function idolIcon(key, type) {
+    return (
         <Card key={key}>
-            <Card.Content textAlign = "center">
+            <Card.Content textAlign="center">
                 <Image
                     centered
                     src={
-                        type === 'cgss'?
-                        baseURL_cgss + key
-                        :
-                        baseURL_mltd + key
+                        type === "cgss"
+                            ? baseURL_cgss + key
+                            : baseURL_mltd + key
                     }
                 />
             </Card.Content>
-            <Card.Content extra textAlign = "center">
-                <form method = "post" 
-                action = {"../core/"+type} 
-                id = {key}>
-                    <input type = "text" name = "key" value = {key} readOnly></input>
-                    <Button type = "submit" positive content = "이 아이콘으로 생성하기"/>
+            <Card.Content extra textAlign="center">
+                <form method="post" action={"../core/" + type} id={key}>
+                    <input type="text" name="key" value={key} readOnly></input>
+                    <Button
+                        type="submit"
+                        positive
+                        content="이 아이콘으로 생성하기"
+                    />
                 </form>
             </Card.Content>
         </Card>
     );
 }
 
-function IdolIconList({iconkeylist, type}){
+function IdolIconList({ iconkeylist, type }) {
     const [activePage, setActivePage] = useState(1);
-    const handleChange = (e, {activePage}) => setActivePage(activePage);
+    const handleChange = (e, { activePage }) => setActivePage(activePage);
 
-    if(iconkeylist){
+    if (iconkeylist) {
         return (
             <div>
-                <Card.Group itemsPerRow = {6} doubling>
-                    {iconkeylist.slice((activePage-1)*12, activePage*12).map(x => idolIcon(x, type))}
+                <Card.Group itemsPerRow={6} doubling>
+                    {iconkeylist
+                        .slice((activePage - 1) * 12, activePage * 12)
+                        .map(x => idolIcon(x, type))}
                 </Card.Group>
-                <br/>
-                <Pagination defaultActivePage = {1} boundaryRange = {0} totalPages = {Math.ceil(iconkeylist.length/12)} 
-                onPageChange = {handleChange}/>
+                <br />
+                <Pagination
+                    defaultActivePage={1}
+                    boundaryRange={0}
+                    totalPages={Math.ceil(iconkeylist.length / 12)}
+                    onPageChange={handleChange}
+                />
             </div>
         );
-    }
-    else return null;
+    } else return null;
 }
 
 export default IdolIconList;
